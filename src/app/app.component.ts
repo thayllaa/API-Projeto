09903e0b8389ae03a379116a2c65c84ac44e4c45
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-20 14:22:34 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = "My Spring Boot Project";
+  name = "My Spring Boot App";
 }
